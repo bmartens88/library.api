ee9572d604127f0ed41f165dd68419018d287ddb
Bas Martens <bmartens1988@kpnmail.nl>
2022-08-03 10:15:30 +0200

    Added dynamic structure framework

---
+++ Library.Api/Endpoints/HelloEndpoints.cs
@@ -0,0 +1,15 @@
+﻿using Library.Api.Endpoints.Internal;
+
+namespace Library.Api.Endpoints;
+
+public class HelloEndpoints : IEndpoints
+{
+    public static void DefineEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapGet("hello", () => "Hello World");
+    }
+
+    public static void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+    }
+}
