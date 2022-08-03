18ae0ff04f0adfa0defeaa35f3ac92c44c1eb0c7
Bas Martens <bmartens1988@kpnmail.nl>
2022-08-03 08:38:56 +0200

    Reverted changes made to project in video "Different ways of structuring"

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -3,7 +3,6 @@ using FluentValidation.Results;
 using Library.Api;
 using Library.Api.Auth;
 using Library.Api.Data;
-using Library.Api.Endpoints;
 using Library.Api.Models;
 using Library.Api.Services;
 using Microsoft.AspNetCore.Cors;
@@ -39,7 +38,7 @@ builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
 builder.Services.AddSingleton<DatabaseInitializer>();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
-builder.Services.AddLibraryEndPoints();
+builder.Services.AddSingleton<IBookService, BookService>();
 
 var app = builder.Build();
 
@@ -50,7 +49,91 @@ app.UseSwaggerUI();
 
 app.UseAuthorization();
 
-app.UseLibraryEndPoints();
+app.MapPost("books",
+        // [Authorize(AuthenticationSchemes = ApiKeySchemeConstants.SchemeName)]
+        // [AllowAnonymous]
+        async (Book book, IBookService bookService, IValidator<Book> validator) =>
+        {
+            var validationResult = await validator.ValidateAsync(book);
+            if (!validationResult.IsValid)
+                return Results.BadRequest(validationResult.Errors);
+
+            var created = await bookService.CreateAsync(book);
+            if (!created)
+                return Results.BadRequest(new List<ValidationFailure>
+                {
+                    new("Isbn", "A book with this ISBN-13 already exists")
+                });
+            return Results.CreatedAtRoute("GetBook", new {isbn = book.Isbn}, book);
+            // return Results.Created($"/books/{book.Isbn}", book);
+        }).WithName("CreateBook")
+    .Accepts<Book>("application/json")
+    .Produces<Book>(201)
+    .Produces<IEnumerable<ValidationFailure>>(400)
+    .WithTags("Books");
+// .AllowAnonymous();
+
+app.MapGet("books", async (IBookService bookService, string? searchTerm) =>
+    {
+        if (searchTerm is not null && !string.IsNullOrWhiteSpace(searchTerm))
+        {
+            var matchedBooks = await bookService.SearchByTitleAsync(searchTerm);
+            return Results.Ok(matchedBooks);
+        }
+
+        var books = await bookService.GetAllAsync();
+        return Results.Ok(books);
+    }).WithName("GetBooks")
+    .Produces<IEnumerable<Book>>(200)
+    .WithTags("Books");
+
+app.MapGet("books/{isbn}", async (string isbn, IBookService bookService) =>
+    {
+        var book = await bookService.GetByIsbnAsync(isbn);
+        return book is not null ? Results.Ok(book) : Results.NotFound();
+    }).WithName("GetBook")
+    .Produces<Book>(200)
+    .Produces(404)
+    .WithTags("Books");
+
+app.MapPut("books/{isbn}", async (string isbn, Book book, IBookService bookService,
+        IValidator<Book> validator) =>
+    {
+        book.Isbn = isbn;
+        var validationResult = await validator.ValidateAsync(book);
+        if (!validationResult.IsValid)
+            return Results.BadRequest(validationResult.Errors);
+        var updated = await bookService.UpdateAsync(book);
+        return updated ? Results.Ok(book) : Results.NotFound();
+    }).WithName("UpdateBook")
+    .Accepts<Book>("application/json")
+    .Produces<Book>(200)
+    .Produces<IEnumerable<ValidationFailure>>(400)
+    .WithTags("Books");
+
+app.MapDelete("books/{isbn}", async (string isbn, IBookService bookService) =>
+    {
+        var deleted = await bookService.DeleteAsync(isbn);
+        return deleted ? Results.NoContent() : Results.NotFound();
+    }).WithName("DeleteBook")
+    .Produces(204)
+    .Produces(404)
+    .WithTags("Books");
+
+app.MapGet("status", [EnableCors("AnyOrigin")]() =>
+{
+    return Results.Extensions.Html(@"<!doctype html>
+<html>
+    <head>
+        <title>Status page</title>
+    </head>
+    <body>
+        <h1>Status</h1>
+        <p>The server is working fine. Bye bye!</p>
+    </body>
+</html>");
+}).ExcludeFromDescription();
+// .RequireCors("AnyOrigin");
 
 // Db init here
 var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
